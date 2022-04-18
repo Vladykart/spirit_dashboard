9c992bfd568c34f480a48b128f60014d69e1b7e5
Vladykart <vladykart@gmail.com>
2022-04-18 11:54:39 +0300

    second

--- apps/video.py
+++ apps/video.py
@@ -48,7 +48,7 @@ def app():
                 time_frame = st.selectbox(
                     "Select weekly or monthly downloads", ("weekly", "monthly", "daily"))
 
-            st.form_submit_button()
+                st.form_submit_button()
 
     st.markdown("### Sample Data")
     query = {'eventAction': {"$in": events_action_selector},
@@ -96,7 +96,7 @@ def app():
                 # Infer basic colDefs from dataframe types
 
                 st.write(df.groupby('week').sum())
-            st.form_submit_button()
+                st.form_submit_button()
         # st.markdown(grid_response['data'].to_html(), unsafe_allow_html=True)
         # st.write(gridOptions)
 
